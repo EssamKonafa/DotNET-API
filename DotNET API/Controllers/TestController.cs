82c155596420e936abec5fa98a869da74d11724b
EssamKonafa <essamazoz9@gmail.com>
2024-10-13 19:26:57 +0300

    Add project files.

---
+++ DotNET API/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNET_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTestController()
+        {
+            var Friendes= new string[] { "essam", "hossam", "diaa" };
+            return Ok(Friendes);
+        }
+    }
+}
